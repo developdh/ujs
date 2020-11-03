d173c5b364a59553316ee764ad6279e93c6baa28
ksw <a@b.com>
2020-11-03 22:23:30 +0900

    add eval code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import socketIo from "socket.io";
+import { evalSafe } from 'eval-safe';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,14 +11,18 @@ const port = 2933;
 
 export function start(){
     app.get('/', (req, res) => {
+        evalSafe('print("a");',{print: console.log});
         res.send('Hello World!');
     });
 
     io.on('connection', (socket) => {
         console.log('a user connected');
-    })
+        socket.on('eval', (data) => {
+            evalSafe(data,{});
+        })
+    });
 
     server.listen(port, () => {
         console.log(`listening on *: ${port}`);
-    })
+    });
 }
