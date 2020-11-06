5f7b5f8a87a3d21cc4f40ba54f93ff640d39dc1e
강성우 <ninggen132@gmail.com>
2020-11-06 17:17:47 +0900

    menu design

--- ujs-parent/src/settingPage/Components/component/Menu.tsx
+++ ujs-parent/src/settingPage/Components/component/Menu.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import CSS from 'csstype';
 
 const Menu = () => {
     const activeStyle = {
         color: '#49c0ec',
-        fontSize: "120%"
     };
+
+    const ul:CSS.Properties = {
+        listStyle: 'none',
+        paddingLeft: '16px',
+        display: 'flex',
+    }
+
+    const li: CSS.Properties = {
+        paddingLeft: '10px',
+        paddingRight: '10px',
+        borderRight: '1px solid gray',
+    }
+
+    const link: CSS.Properties = {
+        textDecoration: 'none',
+    }
 
     return (
         <div>
-            <ul>
-                <li><NavLink exact to="/" activeStyle={activeStyle}>Main</NavLink></li>
-                <li><NavLink to="/setting" activeStyle={activeStyle}>setting</NavLink></li>
-                <li><NavLink to="/info" activeStyle={activeStyle}>info</NavLink></li>
+            <ul style={ul}>
+                <li style={li}><NavLink exact to="/" activeStyle={activeStyle} style={link}>Main</NavLink></li>
+                <li style={li}><NavLink to="/setting" activeStyle={activeStyle} style={link}>setting</NavLink></li>
+                <li style={li}><NavLink to="/info" activeStyle={activeStyle} style={link}>info</NavLink></li>
             </ul>
             <hr/>
         </div>
