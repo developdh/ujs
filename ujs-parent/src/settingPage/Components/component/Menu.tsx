147a58177be49ced5d99190121790d137f828398
강성우 <ninggen132@gmail.com>
2020-11-06 18:02:56 +0900

    add logo

--- ujs-parent/src/settingPage/Components/component/Menu.tsx
+++ ujs-parent/src/settingPage/Components/component/Menu.tsx
@@ -1,37 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import CSS from 'csstype';
+import {Properties as css} from 'csstype';
+import './Menu.css';
 
 const Menu = () => {
     const activeStyle = {
         color: '#49c0ec',
     };
 
-    const ul:CSS.Properties = {
+    const ul: css = {
         listStyle: 'none',
         paddingLeft: '16px',
         display: 'flex',
+        alignItems: "center",
     }
 
-    const li: CSS.Properties = {
+    const li: css = {
         paddingLeft: '10px',
         paddingRight: '10px',
         borderRight: '1px solid gray',
     }
 
-    const link: CSS.Properties = {
+    const link: css = {
         textDecoration: 'none',
     }
 
     return (
-        <div>
+        <nav>
             <ul style={ul}>
+                <li><span id="logo">UJS</span></li>
                 <li style={li}><NavLink exact to="/" activeStyle={activeStyle} style={link}>Main</NavLink></li>
                 <li style={li}><NavLink to="/setting" activeStyle={activeStyle} style={link}>setting</NavLink></li>
                 <li style={li}><NavLink to="/info" activeStyle={activeStyle} style={link}>info</NavLink></li>
             </ul>
             <hr/>
-        </div>
+        </nav>
     );
 };
 
