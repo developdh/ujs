d728b3edb1c2aa865263288a891746d0425d54a7
강성우 <ninggen132@gmail.com>
2020-12-23 09:27:37 +0900

    child_precess remove

--- ujs-parent/src/server/server.ts
+++ ujs-parent/src/server/server.ts
@@ -6,40 +6,43 @@ import indexRouter from './route/index';
 import { ioStart as socketStart } from './socket';
 import bodyParser from 'body-parser';
 
+
 const app = express();
 const server = http.createServer(app);
-export const io = socketIo(server);
+export const io = socketIo(server, { serveClient: false });
 
 const corsOption: cors.CorsOptions = {
     origin: "*",
     optionsSuccessStatus: 200,
 }
 
-const port = 2933;
+export function serverStart(){
+    const port = 2933;
 
-app.use(cors(corsOption));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', indexRouter);
+    app.use(cors(corsOption));
+    app.use(bodyParser.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use('/', indexRouter);
 
-// 에러처리 코드 =========================================================================
-app.use(function (err: any, req: any, res: any, next: any) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // 에러처리 코드 =========================================================================
+    app.use(function (err: any, req: any, res: any, next: any) {
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    res.status(err.status || 500);
-    res.send({
-        error: {
-            code: err.status || 500,
-            message: err.message,
-        }
-    })
-});
+        res.status(err.status || 500);
+        res.send({
+            error: {
+                code: err.status || 500,
+                message: err.message,
+            }
+        })
+    });
 
-// socket.io 코드 =========================================================================
-socketStart();
+    // socket.io 코드 =========================================================================
+    socketStart();
 
-// 서버시작 코드 =========================================================================
-server.listen(port, () => {
-    console.log(`listening on *: ${port}`);
-});
+    // 서버시작 코드 =========================================================================
+    server.listen(port, () => {
+        console.log(`listening on *: ${port}`);
+    });
+}
