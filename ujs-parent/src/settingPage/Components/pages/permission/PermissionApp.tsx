0b07371d4005a1fbdccd46f00cb9725ab4d216c3
developdh <donghee000077@gmail.com>
2020-12-23 14:40:41 +0900

    permissionapp create

--- ujs-parent/src/settingPage/Components/pages/permission/PermissionApp.tsx
+++ ujs-parent/src/settingPage/Components/pages/permission/PermissionApp.tsx
@@ -1,12 +1,55 @@
 import React, { Component, useState } from 'react';
+import sanitizeHtml from 'sanitize-html';
 
-function PermissionApp() {
+const PermissionApp = () => {
     this.state = {
-        
+        permission: [
+            {
+                name: 'example1',
+                checked: false
+            },
+            {
+                name: 'example2',
+                checked: false
+            },
+            {
+                name: 'example3',
+                checked: false
+            },
+            {
+                name: 'example4',
+                checked: false
+            },
+        ],
+        search: ''
     }
+    props: any;
+    handleChange = (e) => {
+        this.setState({
+            [e.target.search]: e.target.value
+        })
+    }
+    const defaultOptions = {
+        allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
+        allowedAttributes: {
+          'a': [ 'href' ]
+        },
+      };
+      
+      const sanitize = (dirty, options) => ({
+        __html: sanitizeHtml(
+          dirty, 
+          { ...defaultOptions, ...options }
+        )
+      });
     return(
         <div>
-
+            <input 
+                placeholder="이름"
+                value={sanitizeHtml(this.state.name)}
+                onChange={this.handleChange}
+                name="name"
+            />
         </div>
     );
 }
